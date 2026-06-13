ac143e67236f31f55f7a9f6aced6ab17771e3b1f
Samriddh Kamde <samriddhkamde1235@gmail.com>
2026-06-13 10:39:37 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Consumer consumer = new Consumer(101, "Rahul", 250);
+
+        System.out.println("----- Sample Bill -----");
+        Consumer sampleConsumer = new Consumer(101, "Rahul", 250);
+
         TariffCalculator calc = new TariffCalculator();
+        calc.displayBill(sampleConsumer);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\n----- Generate Your Own Bill -----");
+
+        System.out.print("Enter Consumer ID: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter Consumer Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Units Consumed: ");
+        int units = sc.nextInt();
+
+        Consumer consumer = new Consumer(id, name, units);
+
+        System.out.println("\n----- Your Bill -----");
         calc.displayBill(consumer);
+
+        sc.close();
     }
 }
